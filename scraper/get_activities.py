a21807b1b10792f21b10123c6c79f41471915712
Gabrie <thegabeman@gmail.com>
2022-03-22 23:03:30 +0100

    Read activities

--- scraper/get_activities.py
+++ scraper/get_activities.py
@@ -1,10 +1,22 @@
+from typing import List
+from datetime import datetime
 import stravatoken
 import requests
 
+## URL to retrieve the first "page" of activities
 url = "https://www.strava.com/api/v3/activities" + "?access_token=" + stravatoken.strava_tokens['access_token']
+## URL to retrieve one specific strava activity, in this example 6865796332 is the activity ID:
+## url = "https://www.strava.com/api/v3/activities/6865796332" + "?access_token=" + stravatoken.strava_tokens['access_token']
+
 
 # Get first 20 activiteis from Strava
 readStrava = requests.get(url)
-readStrava = readStrava.json()
+ListOfActivities = readStrava.json()
 
-print( readStrava)
+for activity in ListOfActivities:
+    if activity["device_watts"]:
+        print( activity["id"], activity["name"],activity["start_date_local"],round(activity["distance"]/1000,2),activity["total_elevation_gain"],activity["trainer"],activity["average_speed"],activity["kilojoules"],activity["workout_type"],activity["device_watts"],activity["weighted_average_watts"], activity["suffer_score"])
+        
+    else:
+        print( activity["id"], activity["name"],activity["start_date_local"],round(activity["distance"]/1000,2),activity["total_elevation_gain"],activity["trainer"],activity["average_speed"],activity["kilojoules"],activity["workout_type"],activity["device_watts"])
+
